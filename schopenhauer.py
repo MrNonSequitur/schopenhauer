07e08f547c39e7e8d3bcfb8f809983bf2fa9ab47
Wyatt S Carpenter <wyattscarpenter@gmail.com>
2016-09-12 18:57:15 -0500

    misc & some unicode nonsense

--- schopenhauer.py
+++ schopenhauer.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
+# coding=utf-8
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep, time
 import json
 import argparse
+import unicodedata
 
 #The Solomon Ucko Memorial Python 2/3 Input Method Compatibility Hack
 try:
 	input = raw_input
-except NameError as e:
-	pass
+except NameError as e: pass
 
 #parse command-line arguments
 parser = argparse.ArgumentParser(description="Schopenhauer is a program that autonomously plays Protobowl, a digital version of Quizbowl, which is a quiz game.")
@@ -67,16 +68,6 @@ if args.be_the_schopenhauer:
 	be_the_schopenhauer = True
 else:
 	be_the_schopenhauer = False
-	
-
-# define various keys to be sent to protobowl as keyboard shortcuts
-pause = 'p'
-resume = 'r'
-#DO NOT use buzz as an alias for Keys.SPACE; that will reference the function "buzz" instead
-next = 'n'
-chat = '/'
-enter = Keys.RETURN
-skip = 's'
 
 # define various methods to be used in interpretting the website
 # the breadcrumb stuff could probably be refactored, but it should work as-is.
@@ -86,17 +77,23 @@ def vprint(text):
 
 def buzz(text):
 	top_bundle = browser.find_elements_by_class_name('bundle')[0] #the top bundle is arbitrarily sent keys because we need to send keys to *something*
-	top_bundle.send_keys(Keys.SPACE)
-	sleep(.5) # let the guess box appear
-	top_bundle.send_keys(text + enter)
-	top_bundle.send_keys("n")
-
+	try:
+		browser.find_element_by_class_name('buzzbtn').click()	
+		vprint("hit buzz button")
+		sleep(.5) # let the guess box appear
+		top_bundle.send_keys(text + '\n')
+	except: vprint("couldn't hit buzz"); top_bundle.send_keys('\n')
+	
+	
 def get_knowledge(i):
 	bundle = browser.find_elements_by_class_name('bundle')[i]
 	qid = bundle.get_attribute("class").split("qid-")[1].split(" ")[0]
 	if i > 0: #this feels inelegant, but works
+		vprint("Now I'm going to get the raw breadcrumb...")
 		raw_breadcrumb = bundle.find_element_by_class_name('breadcrumb').text
+		vprint("raw_breadcrumb: " +raw_breadcrumb)
 		answer = raw_breadcrumb.split("/Edit\n")[1]
+		answer = unicodedata.normalize('NFKD', answer).encode("ascii", "ignore").decode()
 		answer = answer.split("(")[0] #get only the first part of the answer, not the parenthetical note
 		answer = answer.split("[")[0] #get only the first part of the answer, not the parenthetical note
 		answer = answer.strip() #strip whitespace characters from around the answer (presumably in between the parenthetical note and the real answer)
@@ -117,7 +114,7 @@ def get_answer(i):
 	else:
 		return ''
 
-# define methods to deal with guessing
+#define how to work with knowledge
 knowledge = {}
 try:
 	with open(file_in, 'r') as f:
@@ -125,7 +122,6 @@ try:
 		#I'm not a huge fan of json (sexprs are better), but this is better than my original plan of calling eval on whatever we find in knowledge.json and hoping it's a data structure
 except Exception as e:
 	print(str(e))
-	knowledge = {}
 initial_knowledge_length = len(knowledge)
 vprint("knowledge currently consists of "+str(len(knowledge))+" pairs.")
 def guess_answer(qid):
@@ -136,14 +132,13 @@ def guess_answer(qid):
 	return ""
 def record_answer(qid, answer):
 	knowledge[qid] = answer
-def write_out(filename):
+def write_out(filename, object=knowledge):
 	with open(filename, 'w') as f:
-		json.dump(knowledge, f, sort_keys=True) # keys will be sorted to reduce deltas in our version control system
+		json.dump(object, f, sort_keys=True) # keys will be sorted to reduce deltas in our version control system
 
 # navigate to the website
 browser = webdriver.Firefox()
 browser.get(url)
-sleep(1) #let the page render
 while not browser.find_element_by_id('username').is_displayed():
 	sleep(.1) #wait for the page to load
 # set name
@@ -183,40 +178,49 @@ try:
 except Exception as e:
 	vprint("The button couldn't be clicked, assuming it's already pressed...")
 
+#main loop:
+#must use buttons to identify game state, as the state markers are not displayed on some screen sizes
+#must use try click because is_displayed doesn't seem to work the way we want it to.
 try:
-	answered = 0
-	while times != 0: # deliberately allow negative numbers to cause it to loop forever
-		curr_well = browser.find_element_by_class_name('well')#track well to determine the current question. the well is arbitrarily sent keys because we need to send keys to *something*
-		times-=1
-		if browser.find_element_by_class_name('finished').is_displayed():
-			curr_well.send_keys(next)
-			curr_well = browser.find_element_by_class_name('well')
-		k = get_knowledge(0)
-		guess = guess_answer(k['qid']) #this should return an empty string if there is no answer, b/c I can't be bothered to learn error handling		
-		if browser.find_elements_by_class_name('pause')[1].is_displayed(): # must match the label [1] not the button [0]
-			curr_well.send_keys(resume)
-		if guess == '':
-			if browser.find_element_by_class_name('pausebtn').is_displayed():
-				curr_well.send_keys(skip)
-			else: # Schopenhauer is probably chatting accidentally
-				buzz("Franz Liszt")
-		else:
-			vprint(str(times)+"("+str(time())+")"+": "+k['qid']+":"+guess)
-			buzz(guess)
-			answered+=1
-		sleep(delay) #this is ugly, but it works
-		try:
-			a = get_knowledge(1)
-			record_answer(a['qid'], a['answer'])
-		except Exception as e:
-			vprint("When getting knowledge: "+str(e))
-		if times % 100 == 0 and centennial:
-			vprint("Only "+str(times)+" times left to go. Writing out knowledge to " + str(file_out)+"... ("+str(len(knowledge))+" pairs)")
-			write_out(file_out)
-		
-	vprint("knowledge is now "+str(len(knowledge))+ " pairs, which is "+str(len(knowledge)-initial_knowledge_length)+" more than it was intially.")
-	vprint("Schopenhauer tried to answer a question "+str(answered)+" times")
-	write_out(file_out)
+	try:
+		answered = 0
+		while times != 0: # deliberately allow negative numbers to cause it to loop forever
+			times-=1
+			try:
+				browser.find_element_by_class_name('resume').click()	
+				vprint("hit resume button")
+			except: vprint("couldn't hit resume")
+			curr_well = browser.find_element_by_class_name('well')#track well to determine the current question. the well is arbitrarily sent keys because we need to send keys to *something*
+			k = get_knowledge(0)
+			guess = guess_answer(k['qid']) #this should return an empty string if there is no answer
+			if guess == '':
+				try:
+					browser.find_element_by_class_name('skipbtn').click()	
+					vprint("hit skip button")
+				except: vprint("couldn't hit skip")
+			else:
+				buzz(guess)
+				vprint(str(times)+"("+str(time())+")"+": "+k['qid']+":"+guess)
+				answered+=1
+			try:
+				a = get_knowledge(1)
+				record_answer(a['qid'], a['answer'])
+			except Exception as e:
+				vprint("When getting knowledge: "+str(e))
+			if times % 100 == 0 and centennial:
+				vprint("Only "+str(times)+" times left to go. Writing out knowledge to " + str(file_out)+"... ("+str(len(knowledge))+" pairs)")
+				write_out(file_out)
+			sleep(delay)
+			try:
+				browser.find_element_by_class_name('nextbtn').click()	
+				vprint("hit next button")
+			except: vprint("couldn't hit next")
+			
+		vprint("knowledge is now "+str(len(knowledge))+ " pairs, which is "+str(len(knowledge)-initial_knowledge_length)+" more than it was intially.")
+		vprint("Schopenhauer tried to answer a question "+str(answered)+" times")
+		write_out(file_out)
+	except Exception as e:
+		vprint(str(e))
 except:
 	vprint("exception in main loop.\nAborting...")
 	try:
